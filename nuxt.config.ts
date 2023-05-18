7c608876bb8fd04a91d34d120776ec00ed37f757
Sajjad Mirshabi <m.sajjad1378@gmail.com>
2023-05-18 13:18:08 +0330

    Ulearnit V1.0.1 - Change Api URL - Fix Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
     },
     vite: {
        server: {
-         proxy: { "/api": "http://api.ulearnit.ir/" },
+         proxy: { "/api": "https://sajjadmirshabi.ir" },
        },
     },
 })
--- utilities/ApiUrls.ts
+++ utilities/ApiUrls.ts
@@ -1,4 +1,4 @@
-export const ApiUrl = "http://api.ulearnit.ir";
+export const ApiUrl = "https://sajjadmirshabi.ir";
 //export const ApiUrl = "http://localhost:5161";
 export const FtpUrl = "http://dl.ulearnit.ir";
 //export const ApiUrl = "http://localhost:5000";
